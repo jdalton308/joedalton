fb6c797c45a92001ea28e6155bcb53c4f25cb809
Joe Dalton <joe@sequence.com>
2015-10-26 14:05:12 -0700

    Began messing with D3

---
+++ js/src/d-three.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    var chart = d3.select('.chart');
+    var bars = chart.selectAll('div');
+    var data = [12, 30, 9, 47, 80, 64, 100];
+
+    bars.data(data)
+            .enter().append('div')
+                .classed('bar', true)
+                .style('width', function(d){ return d + '%'; })
+                .text(function(d){ return d });
+}); // end doc ready
