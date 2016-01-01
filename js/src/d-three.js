7b5491d065fbbd3c00ea9967a91519577c724d3e
jdalton308 <jdalton308@yahoo.com>
2015-12-31 23:14:10 -0800

    Built the rest of the work example pages, and the shophouse riddle subdomain

--- js/src/d-three.js
+++ js/src/d-three.js
@@ -1,12 +1,12 @@
-$(function(){
+// $(function(){
 
-    var chart = d3.select('.chart');
-    var bars = chart.selectAll('div');
-    var data = [12, 30, 9, 47, 80, 64, 100];
+//     var chart = d3.select('.chart');
+//     var bars = chart.selectAll('div');
+//     var data = [12, 30, 9, 47, 80, 64, 100];
 
-    bars.data(data)
-            .enter().append('div')
-                .classed('bar', true)
-                .style('width', function(d){ return d + '%'; })
-                .text(function(d){ return d });
-}); // end doc ready
+//     bars.data(data)
+//             .enter().append('div')
+//                 .classed('bar', true)
+//                 .style('width', function(d){ return d + '%'; })
+//                 .text(function(d){ return d });
+// });
