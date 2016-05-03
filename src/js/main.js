c232a5922c8432409448287d9f519992a2f11daf
Joe Dalton <joe@sequence.com>
2016-05-02 21:48:05 -0700

    Nav arrows working, Sessionstorage, and page placeholders

--- src/js/main.js
+++ src/js/main.js
@@ -3,52 +3,76 @@ $(function(){
   // Home screen
   var Landing = (function(){
 
+    var $body = $('body');
     var $start = $('.start-btn');
+    var $cont = $start.parents('.text-cont');
     var $nextBtn = $('.next-arrow');
     var $logo = $('.logo');
 
     function init() {
-      $start.click(function(){
-        var $this = $(this);
-        var $cont = $this.parents('.text-cont');
-        var $line1 = $this.siblings('.line-1');
-        var $line2 = $this.siblings('.line-2');
-        var $line3 = $this.siblings('.line-3');
-        
+      var beenBefore = (window.sessionStorage.joedalton === 'true');
+
+      if (beenBefore) {
         $cont.addClass('start');
+        $logo.addClass('show');
+        $nextBtn.addClass('show');
+      } else {
 
-        // show first line of text
-        window.setTimeout(function(){
-          $line1.addClass('start');
+        // Bind starting animation event
+        $start.click(function(){
+          var $this = $(this);
+          var $cont = $start.parents('.text-cont');
+          var $line1 = $this.siblings('.line-1');
+          var $line2 = $this.siblings('.line-2');
+          var $line3 = $this.siblings('.line-3');
           
-          // wait 2.8s, then show second line of text
+          $cont.addClass('start');
+
+          // show first line of text
           window.setTimeout(function(){
-            $line2.addClass('start');
-
-            // wait 2s, hide text
+            $line1.addClass('start');
+            
+            // wait 2.8s, then show second line of text
             window.setTimeout(function(){
-              $line1.addClass('hide');
-              $line2.addClass('hide');
+              $line2.addClass('start');
 
-              // wait, 1s, show last line, and arrows
+              // wait 2s, hide text
               window.setTimeout(function(){
-                $cont.addClass('end');
-                $line1.hide();
-                $line2.hide();
-                // $line3.addClass('start');
+                $line1.addClass('hide');
+                $line2.addClass('hide');
 
-                // wait 0.5s, show logo
-                window.setTimeout(function(){ // stop here /////
-                  $logo.addClass('show');
-                  $nextBtn.addClass('show');
-                }, 500);
-              }, 1200);
-            }, 3000)
-          }, 2800);
-        }, 1000);
-        
+                // wait, 1s, show last line, and arrows
+                window.setTimeout(function(){
+                  $cont.addClass('end');
+                  $line1.hide();
+                  $line2.hide();
+                  // $line3.addClass('start');
+
+                  // wait 0.5s, show logo
+                  window.setTimeout(function(){ // stop here /////
+                    $logo.addClass('show');
+                    $nextBtn.addClass('show');
+                  }, 500);
+                }, 1200);
+              }, 3000)
+            }, 2800);
+          }, 1000);
+
+          // remember that user was here
+          window.sessionStorage.setItem('joedalton', 'true');
+        });
+      }
+
+      // Bind nav-arrow events
+      $nextBtn.click(function(){
+        var $this = $(this);
+        var target = $this.data('target');
+
+        var $targetSection = $('#section-'+ target);
+        $targetSection.addClass('focus');
       });
-    }
+        
+    } // end init()
 
     return {
       init: init
@@ -56,5 +80,11 @@ $(function(){
   })();
 
   Landing.init();
+
+
+  // Back Bars
+  $('.back-bar').click(function(){
+    $(this).closest('.side-section').removeClass('focus');
+  });
 
 });
