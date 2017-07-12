7ea4735239b9e3328aafc2f00cb8e3b99c285250
jdalton <jdalton@lifetimefitness.com>
2017-07-11 21:04:31 -0600

    Project card animation opening and closing

--- src/js/main.js
+++ src/js/main.js
@@ -70,46 +70,99 @@ $(function(){
 
 
 
-  // Work Section
+  // Projects Section
   //----------------
-  var Work = (function(){
-    var $section = $('#section-work');
-    var $workItems = $section.find('.work-item');
-    var $workCont = $section.find('.work-container');
-    var $workDetails = $workCont.find('.work-item-details');
-    var $closeBtn = $workCont.find('.close-icon');
+  var Projects = (function(){
+    var $projectItems = $('.portfolio-item');
+    // var sourceCard = null;
 
-    function getWork( name ) {
-      return $.ajax({
-        url: '/work/' + name + '.html',
-        type: 'get'
+
+    function focusItem($el) {      
+      var windowHeight = window.innerHeight;
+      var boundingRect = $el[0].getBoundingClientRect();
+      var clonedStartStyles = {
+        top: boundingRect.top,
+        left: boundingRect.left,
+        width: boundingRect.width,
+        height: boundingRect.height,
+      };
+
+      var clonedEndStyles = {
+        top: '40px',
+        width: 780,
+        height: (windowHeight - 80),
+        boxShadow: '0 0 200px 200px rgba(0,0,0,0.8)',
+      };
+
+      var $clonedCard = $el.clone().addClass('focused-item').css(clonedStartStyles);
+
+      // - Add element
+      $body.append($clonedCard);
+
+      // - Animate element
+      window.setTimeout(function(){
+        $clonedCard.addClass('show').css(clonedEndStyles);
+      }, 100);
+
+      // - Bind close events
+      bindCloseEvents($clonedCard, $el);
+    }
+
+
+    function bindCloseEvents($el, $sourceCard) {
+      $el.click(function(e){
+        e.stopPropagation();
+      });
+
+      var $closeIcon = $el.find('.close-icon');
+      $closeIcon.click(function(e){
+        e.stopPropagation();
+        collapseItem($el, $sourceCard);
+      });
+
+      $body.click(function(){
+        collapseItem($el, $sourceCard);
       });
     }
-    function closeWork() {
-      $workCont.removeClass('show');
+
+
+    function collapseItem($el, $targetEl) {
+      var targetBounding = $targetEl[0].getBoundingClientRect();
+      var collapseStyle = {
+        top: targetBounding.top,
+        left: targetBounding.left,
+        height: targetBounding.height,
+        width: targetBounding.width,
+        boxShadow: '0 0 5px #000',
+      };
+
+      $el.addClass('collapse').css(collapseStyle);
+
+      window.setTimeout(function(){
+        $el.remove();
+      }, 800);
     }
+
+
+    function bindEvents() {
+      $projectItems.click(function(e){
+        e.stopPropagation();
+        focusItem($(this));
+      });
+    }
+
 
     function init() {
-      $workItems.click(function(){
-        var $this = $(this);
-        var name = $this.data('work');
-        getWork( name ).then(function(data){
-          $workDetails.html(data);
-          $workCont.addClass('show');
-        });
-      });
-
-      $closeBtn.click(function(){
-        closeWork();
-      });
+      bindEvents();
     }
 
     return {
       init: init
     };
+
   })();
 
-  Work.init();
+  Projects.init();
 
 
 });
